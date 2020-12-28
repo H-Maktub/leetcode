9f19d621d2e45a4675f78d537e5bc05ed459dac8
maktub <maktub@live.cn>
2020-12-29 00:02:44 +0800

    20

---
+++ 20.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        dict = {'(':')','{':'}','[':']'}
+        list = []
+        for ch in s:
+            if dict.get(ch) != None:
+                list.append(dict[ch])
+            else:
+                if len(list)==0 or list.pop() != ch:
+                    return False
+        b = bool(len(list))
+        return bool(1 - b)
+
+if __name__ == "__main__":
+    #106+1100
+    a = Solution.isValid("",s="()")
+    print ('aa',a)
+    pass
