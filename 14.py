84a3deec47ef76d59be0da58682c79f85c751b32
黄坤强 <787013063@qq.com>
2020-12-28 18:08:22 +0800

    update

--- 14.py
+++ 14.py
@@ -1,12 +1,32 @@
-from _typeshed import StrPath
 from typing import List
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
+        mainStr = strs[0]
+        
         for str in strs:
-            
-        return ""
+            if len(mainStr) > len(str):
+                mainStr = str
+        strs.remove(mainStr)
+        right = len(mainStr)
+        current = 0
+        
+        while current != right:
+            cache = mainStr[:right]
+            result = True
+            for str in strs:
+                if cache != str[:right]:
+                    result = False
+                    break
+            if result:
+                right = current//2 + right//2
+                current = right
+            else:
+                right = right//2
+
+        
+        return mainStr[:right]
 if __name__ == "__main__":
     #106+1100
-    a = Solution.longestCommonPrefix("",StrPath=["flower","flow","flight"])
+    a = Solution.longestCommonPrefix("",strs=["flower","flow","flight"])
     print ('aa',a)
     pass
