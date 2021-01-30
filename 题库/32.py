0e21d4b0969894f6a949b36cfa85e26fb5dce9c2
maktub <maktub@live.cn>
2021-01-30 12:26:39 +0800

    2

--- 题库/32.py
+++ 题库/32.py
@@ -1,22 +1,49 @@
+# class Solution:
+#     def longestValidParentheses(self, s: str) -> int:
+#         temp = [0]
+#         ret = 0
+#         t=1
+#         for i,ch in enumerate(s):
+#             if ch =="(":
+#                 t+=1
+#                 temp.append(i+1)
+#             else:
+#                 t-=1
+#                 temp.pop()
+#             if t == 0:
+#                 temp.append(i+1)
+#                 t=1
+#             else:
+#                 ret = max(ret,i+1-temp[-1])
+#         return ret
+
 class Solution:
     def longestValidParentheses(self, s: str) -> int:
-        temp = [0]
-        t = 0
-        ret = 0
-        for i,ch in enumerate(s):
-            if ch =="(":
-                temp.append(temp[i]+1)
-            else:
-                t-=1
-                temp.append(temp[i]-1)
+        l = len(s)
+        if l == 0:return 0
+        temp=[0] *l
+        for i in range(1,l):
+            if s[i] == ")" and s[i-1] =="(":
+                temp[i] = temp[i-2]+2
+            elif s[i] == ")" and s[i-1] ==")" and i-temp[i-1]-1>=0 and s[i-temp[i-1]-1]=="(":
+                temp[i]= temp[i-1]+temp[i-temp[i-1]-2]+2
         print(temp)
-        return ret
-
+        return max(temp)
 if __name__ == "__main__":
     a = Solution()
-    b = a.longestValidParentheses("(()")
+    b = a.longestValidParentheses("(()")#2
     print("=====",b)
-    b = a.longestValidParentheses(")()())")
+    b = a.longestValidParentheses(")()())")#4
     print("=====",b)
-    b = a.longestValidParentheses("()(()")
+    b = a.longestValidParentheses("()(()")#2
+    print("=====",b)
+    b = a.longestValidParentheses(")")#0
+    print("=====",b)
+    b = a.longestValidParentheses(")()())")#4
+    print("=====",b)
+    b = a.longestValidParentheses("(()(((()")#2
+    print("=====",b)
+    b = a.longestValidParentheses("()")#2
+    print("=====",b)
+    b = a.longestValidParentheses("()(())")#6
     print("=====",b)
---
+++ 题库/37.py
@@ -0,0 +1,43 @@
+from typing import List
+class Solution:
+    def solveSudoku(self, board: List[List[str]]) -> None:
+        rows = [set() for i in range(9)]
+        columns = [set() for i in range(9)]
+        boxes = [set() for i in range(9)]
+        for i,temp in enumerate(board):
+            for j,num in enumerate(temp):
+                if num != ".":
+                    if num in rows[i]:
+                        return False
+                    else:
+                        rows[i].add(num)
+
+                    if num in columns[j]:
+                        return False
+                    else:
+                        columns[j].add(num)
+                    n = i//3*3+j//3
+                    # print(n,i+1,i//3,(j+1)//3)
+                    if num in boxes[n]:
+                        return False
+                    else:
+                        boxes[n].add(num)
+        print(rows)
+        print(columns)
+        print(boxes)
+
+
+if __name__ == "__main__":
+    a = Solution()
+    b = a.solveSudoku([
+  ["5","3",".",".","7",".",".",".","."],
+  ["6",".",".","1","9","5",".",".","."],
+  [".","9","8",".",".",".",".","6","."],
+  ["8",".",".",".","6",".",".",".","3"],
+  ["4",".",".","8",".","3",".",".","1"],
+  ["7",".",".",".","2",".",".",".","6"],
+  [".","6",".",".",".",".","2","8","."],
+  [".",".",".","4","1","9",".",".","5"],
+  [".",".",".",".","8",".",".","7","9"]
+])
+    print(b)
